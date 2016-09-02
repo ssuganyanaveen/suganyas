59f0d13dc0787f497ba3dba142712eae1e1eca29
ssuganyanaveen <ssugannavi@gmail.com>
2016-09-02 10:56:13 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -3,13 +3,13 @@ public class positive
 {
 public static void main(string args[])
 {
-scanner sc=new scanner(system.in);
+Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 if(n>0)
 System.out.println("positive");
-elseif(n<0)
+else if(n<0)
 System.out.println("negative");
-else(n==0)
+else
 System.out.println("zero");
 }
 }
